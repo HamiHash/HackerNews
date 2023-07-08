03e9cc584503dc904d58bcd4be0ecfe08cbf0e8f
hamihash <hami.hash@yahoo.com>
2023-07-08 21:32:40 +0330

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Hamed Hashemi on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
